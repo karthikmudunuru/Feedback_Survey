d9e504719d34df310717d45992531eb2d189c0bc
karthikmudunuru <61002494+karthikmudunuru@users.noreply.github.com>
2020-02-12 23:46:42 -0800

    Add files via upload

---
+++ Feedback-Survey-master/Feedback-Survey-master/server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("../config/keys");
+require("../models/user");
+require("../services/passport");
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(err => {
+    console.log(
+      `Error occurred while connecting to mongo db server. Error: ${err}`
+    );
+  });
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 2592000000, //30 days = 30 * 24 hrs = 30 * 24 * 60 min = 30 * 24 * 60 * 60 sec = 30 * 24 * 60 * 60 * 1000 ms
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize(null));
+app.use(passport.session(null));
+
+require("../routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
